04e628a5200af79393805d4054995aaa722b109f
pdp <john.lgd65@gmail.com>
2023-04-04 17:02:36 +0500

    gogo

--- src/main/java/dev/jlkeesh/post/PostRepository.java
+++ src/main/java/dev/jlkeesh/post/PostRepository.java
@@ -1,7 +1,11 @@
 package dev.jlkeesh.post;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(path = "postlar", collectionResourceRel = "postlar")
-public interface PostRepository extends JpaRepository<Post, Integer> {}
+public interface PostRepository extends JpaRepository<Post, Integer> {
+    Post findByTitleIgnoreCase(String title);
+}
