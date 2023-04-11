1a77b46461500409f9c5aa73599d2ee2327526fa
pdp <john.lgd65@gmail.com>
2023-04-11 16:18:25 +0500

    gogo

--- src/main/java/dev/jlkeesh/Main.java
+++ src/main/java/dev/jlkeesh/Main.java
@@ -3,33 +3,15 @@ package dev.jlkeesh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.task.TaskExecutor;
-import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-
-import java.util.concurrent.Executor;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableAsync
+@EnableScheduling
 public class Main {
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
-
-    @Bean(name = "your")
-    public TaskExecutor your() {
-        ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
-        taskExecutor.setCorePoolSize(10);
-        taskExecutor.setMaxPoolSize(40);
-        taskExecutor.setKeepAliveSeconds(60);
-        taskExecutor.setQueueCapacity(100);
-        taskExecutor.setThreadNamePrefix("manguberdibek-");
-        taskExecutor.initialize();
-        return taskExecutor;
-    }
-
 }
---
+++ src/main/java/dev/jlkeesh/SchedulerService.java
@@ -0,0 +1,66 @@
+package dev.jlkeesh;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+@Component
+@ConditionalOnProperty(name = "scheduler.enable",
+        matchIfMissing = true,
+        havingValue = "0"
+)
+@Slf4j
+public class SchedulerService {
+
+    /*private final EntityManager entityManager;*/
+
+    //@Scheduled(fixedDelay = 2, timeUnit = TimeUnit.SECONDS)
+    public void sendNotification() throws InterruptedException {
+        log.info("Notification (Fixed Delay) : " + new Date());
+        TimeUnit.MILLISECONDS.sleep(500);
+    }
+
+    // @Scheduled(fixedRate = 2, timeUnit = TimeUnit.SECONDS)
+    public void sendNotification2() throws InterruptedException {
+        log.info("Notification (Fixed Rate) : " + new Date());
+        TimeUnit.MILLISECONDS.sleep(4000);
+    }
+
+    // @Scheduled(initialDelay = 3, fixedRate = 2, timeUnit = TimeUnit.SECONDS)
+    public void sendNotification3() throws InterruptedException {
+        log.info("Notification (Fixed Rate + Initial Delay) : " + new Date());
+        /*TimeUnit.MILLISECONDS.sleep(4000);*/
+    }
+
+    // @Scheduled(fixedRate = 1, timeUnit = TimeUnit.MINUTES)
+    public void sendNotification4() throws InterruptedException {
+        log.info("Pull Blocked users...................");
+        log.info("Check Blocked Till Expired Or Not...................");
+        log.info("Delete From Block List...................");
+        log.info("set zero to login try count field...................");
+    }
+
+    //@Scheduled(cron = "10-40/5 50-55/1 * * * *")
+    public void sendNotification5() throws InterruptedException {
+        log.info("Pull Blocked users...................");
+    }
+
+    @Async
+    @Scheduled(cron = "59 17 16 L-19 * ?")
+    public void sendNotification6() throws InterruptedException {
+        log.info("Pull Blocked users...................");
+    }
+
+    @Scheduled(cron = "@daily")
+    public void sendNotification7() throws InterruptedException {
+        log.info("Daily Pull Blocked users...................");
+    }
+
+
+
+}
